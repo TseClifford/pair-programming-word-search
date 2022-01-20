8dd47052a8f3a6b7a46e53ddf265478e3d9d6070
Clifford Tse <TseClifford@gmail.com>
2022-01-20 02:25:43 +0000

    fixed wordsearch function, added horizontal, vertical and reverse horizontal checks

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,49 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const reverseHorizontalJoin = reverse(letters).map(ls => ls.join(''));
+  for (let l of reverseHorizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let row = matrix[0].length;
+  let newMatrix = [];
+  
+  for (let i = 0; i < row; i++) {
+    newMatrix[i] = [];
+  }
+  
+  for (let x in matrix) {
+    for (let y in matrix[x]) {
+      newMatrix[y][x] = (matrix[x][y]);
+    }
+  }
+  return newMatrix;
+};
+
+const reverse = function(matrix) {
+  let row = matrix.length;
+  let newMatrix = [];
+  
+  for (let i = 0; i < row; i++) {
+    newMatrix[i] = [];
+  }
+  
+  for (let x in matrix) {
+    for (let j = matrix[x].length - 1; j > 0; j--) {
+      newMatrix[x][matrix[x].length - j - 1] = (matrix[x][j]);
+    }
+  }
+  return newMatrix;
+};
+
+module.exports = wordSearch;
